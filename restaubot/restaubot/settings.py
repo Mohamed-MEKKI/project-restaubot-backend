328115b501aa62f2084d7f84e11006e3590d802f
mohamed.mekki <mohamed.mekki@edu.ece.fr>
2026-05-17 00:47:23 +0200

    update docker file

--- restaubot/restaubot/settings.py
+++ restaubot/restaubot/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -158,6 +159,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:8000",
     "http://127.0.0.1:3000",
     "http://127.0.0.1:8000",
+    ".railway.app",
     "https://restobot-aiqfgv3er-mohamedmekkis-projects.vercel.app",
 ]
 
